64eebb3792974cd031734674f2e6d96ebf0ae954
13th <pavel.voronin@ligastavok.ru>
2024-05-22 18:03:10 +0300

    jUnit5

--- src/test/java/com/example/TestBase.java
+++ src/test/java/com/example/TestBase.java
@@ -4,9 +4,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
 
-@ExtendWith(GlobalCallBack.class)
 public class TestBase {
 
     public static ImportantManager im;
