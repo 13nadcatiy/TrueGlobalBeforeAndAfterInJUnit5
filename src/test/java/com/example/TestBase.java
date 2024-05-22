a097a8765fda2b29bb13e5c26b3d6ba16e41a905
13th <pavel.voronin@ligastavok.ru>
2024-05-22 17:49:27 +0300

    TestNG

--- src/test/java/com/example/TestBase.java
+++ src/test/java/com/example/TestBase.java
@@ -1,10 +1,5 @@
 package com.example;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
-
 public class TestBase {
 
     public static ImportantManager im;
