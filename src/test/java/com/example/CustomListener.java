fa833fbe4ba8fb4dfd1f73b3fffcbf6bdb488cc6
voronin-pavel <pavel.voronin@ligastavok.ru>
2024-05-23 17:24:41 +0300

    Успех!

--- src/test/java/com/example/CustomListener.java
+++ src/test/java/com/example/CustomListener.java
@@ -6,11 +6,13 @@ import org.junit.platform.launcher.TestPlan;
 public class CustomListener extends TestBase implements TestExecutionListener {
     @Override
     public void testPlanExecutionStarted(TestPlan testPlan) {
+        //тут вызывать все что надо перед тестами
         im = new ImportantManager();
     }
 
     @Override
     public void testPlanExecutionFinished(TestPlan testPlan) {
+        //тут вызывать все что надо после тестов
         im.stop();
         im = null;
     }
